9d88c19e0a37b0f3a9cdc5e8ecb3328a0810348d
Jaki Widby and Keturah Howard <keturah@>
2020-01-15 16:38:21 -0800

    create other user-interfacce functions that will take form inputs and perform buisness logic equations to output the correct result depending on which button is selected

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,36 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() { 
   $("form#add").submit(function(event) {
-  event.preventDefault(); 
+    event.preventDefault(); 
 
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
   });
 
-  $("form#add")
-});
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+
+    var number1 = parseInt($("#sub-1").val());
+    var number2 = parseInt($("#sub-2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiplication").submit(function(event) {
+    event.preventDefault();
+
+    var number1 = parseInt($("#multi-1").val());
+    var number2 = parseInt($("#multi-2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+
+    var number1 = parseInt($("#divide-1").val());
+    var number2 = parseInt($("#divide-2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+}); 
